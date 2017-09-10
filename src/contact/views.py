b4ac74695180e7cfc63d0e786202eddab648bf82
Thebreak35 <3gthebreak@gmail.com>
2017-09-10 22:58:52 +0700

    finally

--- src/contact/views.py
+++ src/contact/views.py
@@ -9,7 +9,7 @@ from .forms import contactForm
 def contact(request):
 	title = 'Contact'
 	form = contactForm(request.POST or None)
-	context = {'title':title, 'form':form,}
+	confirm_message = None
 
 	if form.is_valid():
 		name = form.cleaned_data['name']
@@ -20,6 +20,10 @@ def contact(request):
 		emailTo = [settings.EMAIL_HOST_USER]
 		fail_silently=True
 		send_mail = (subject, message, emailFrom, emailTo, fail_silently)
+		title = "Thanks"
+		confirm_message = "Thanks for Contact"
+		form = None
 
+	context = {'title':title, 'form':form, 'confirm_message':confirm_message,}
 	template = 'contact.html'
 	return render(request,template,context)
--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,10 +1,17 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
 # Register your models here.
-from .models import profiles
+from .models import profiles, userStripe
 
 class profileAdmin(admin.ModelAdmin):
 	class Meta:
 		model = profiles
 
-admin.site.register(profiles, profileAdmin)
+admin.site.register(profiles, profileAdmin) 
+
+class userStripeAdmin(admin.ModelAdmin):
+    class Meta:
+        model = userStripe
+admin.site.register(userStripe, userStripeAdmin)
